6c9610724b07ceac453dae6f8c0d14b447501c70
anhkhoaIT <super.khoa14@gmail.com>
2025-04-23 13:37:15 +0700

    render view and config

---
+++ src/main/java/vn/anhkhoaIT/laptopshop/controller/UserController.java
@@ -0,0 +1,34 @@
+package vn.anhkhoaIT.laptopshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.anhkhoaIT.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "hello";
+    }
+}
+
+// @RestController
+// public class UserController {
+//     //Dependency Injection
+//     private UserService userService;
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+//     @GetMapping("/")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+// }
